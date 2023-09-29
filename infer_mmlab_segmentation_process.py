5fe3187b4379bb15bf56220782a5b8c30f517ec7
Ludovic Barusseau <ludovic.barusseau@ikomia.com>
2023-09-29 09:51:02 +0200

    Chore: add metadata fields (algo type and algo tasks)

--- infer_mmlab_segmentation_process.py
+++ infer_mmlab_segmentation_process.py
@@ -185,6 +185,8 @@ class InferMmlabSegmentationFactory(dataprocess.CTaskFactory):
         self.info.original_repository = "https://github.com/open-mmlab/mmsegmentation"
         # Keywords used for search
         self.info.keywords = "mmlab, train, segmentation"
+        self.info.algo_type = core.AlgoType.INFER
+        self.info.algo_tasks = "INSTANCE_SEGMENTATION,SEMANTIC_SEGMENTATION,PANOPTIC_SEGMENTATION"
 
     def create(self, param=None):
         # Create process object
