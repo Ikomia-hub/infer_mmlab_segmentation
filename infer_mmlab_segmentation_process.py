0b35ff421f2a7c3ee86bab092aadc887510ff7f4
allankouidri <allan.kouidri@gmail.com>
2026-06-30 12:12:44 +0200

    Set Python version max = 3.11

--- infer_mmlab_segmentation_process.py
+++ infer_mmlab_segmentation_process.py
@@ -217,6 +217,7 @@ class InferMmlabSegmentationFactory(dataprocess.CTaskFactory):
         self.info.icon_path = "icons/mmlab.png"
         self.info.version = "3.1.0"
         self.info.min_python_version = "3.10"
+        self.info.max_python_version = "3.11.0"
         self.info.min_ikomia_version = "0.16.0"
         # self.info.icon_path = "your path to a specific icon"
         self.info.authors = "MMSegmentation Contributors"
